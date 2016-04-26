253be5503ec7d9290a4f9e0469b1b542b7d6952b
Cristóbal Ugarte <cristobalugarte6@gmail.com>
2016-04-26 11:22:26 -0300

    Mail change

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -49,12 +49,40 @@ class CursoController extends Controller
     
     $curso->save();
 
-    $to = 'Paselramo@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $to = 'cristobalugarte6@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
     $email_subject = "Solicitud de inscripción";
-    $email_body = "El alumno $request->Nombre quiere inscribir un ramo.\n\n"."Su mail es: $request->Mail\n\nTelefono: $request->Telefono\n";
+    $email_body = "El alumno $request->Nombre quiere inscribir el ramo $curso->curso de la Universidad $Uni->Nombre.\n\n"."Su mail es: $request->Mail\n\nTelefono: $request->Telefono\n";
     $headers = "From: noreply@ypaselramo.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
     $headers .= "Reply-To: crugarte@alumnos.uai.cl";    
     mail($to,$email_subject,$email_body,$headers);
+
+    $to = "$request->Mail"; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $email_subject = "Inscripción de ramo";
+    $email_body= 
+    "Estimado/a $request->Nombre:    \n
+    Información de reserva de curso $curso->curso:\n
+    Universidad: $Uni->Nombre \n
+    Lugar: $curso->ubicacion\n
+    Día: $curso->day\n
+    Hora: $curso->horario\n
+    En los próximos minutos un coordinador de PASELRAMO se contactará. \n
+    Posees 4 horas para pagar el curso vía transferencia bancaria, los datos del depósito se detallan a continuación.\n
+    Nombre: Felipe Ignacio Ruiz Bezares \n
+    Rut: 18.957.386-3\n
+    Banco: Santander\n
+    Tipo de cuenta: Cuenta Vista\n
+    Número de cuenta: 0-070-17-22445-4\n
+    Correo: paselramo@gmail.com\n\n
+ 
+    Gracias por elegirnos.\n
+    PASELRAMO";
+
+    $headers = "From: noreply@ypaselramo.com\n"; 
+    $headers .= "Reply-To: paselramo@gmail.com ";    
+    mail($to,$email_subject,$email_body,$headers);
+
+    window.open("http://www.laravel.com", "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=500,left=500,width=400, height=400");
+
     return redirect()->action('UniversitysController@show',[$Uni->id."#about"]);
 	}
 
